3121ebfa708c45f5efd22cfd1bb759487c4e5cb9
Cássio <cassio@MacBook-Pro-de-Cassio.local>
2013-07-26 19:52:48 -0300

    Initial Commit

---
+++ TesteBanco/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  TesteBanco
+//
+//  Created by Cássio on 26/07/13.
+//  Copyright (c) 2013 Wisdomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
